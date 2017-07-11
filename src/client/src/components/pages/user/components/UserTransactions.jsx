5984f4908af05f2052444cb4fe9b98ccaea0c9da
Antti Kivimäki <antti.kivimaki@eatech.fi>
2017-07-11 16:12:55 +0300

    Make transactions more reactive

--- src/client/src/components/pages/user/components/UserTransactions.jsx
+++ src/client/src/components/pages/user/components/UserTransactions.jsx
@@ -2,7 +2,11 @@
 import React from 'react';
 import Card from '../../../common/Card';
 
-type UserTransactionsProps = {
+import * as _ from 'lodash';
+
+type DefaultProps = {}
+
+type Props = {
   makeTransaction: Function,
   user: {
     username: string;
@@ -10,20 +14,49 @@ type UserTransactionsProps = {
   }
 }
 
-const UserTransactions = (props: UserTransactionsProps) => {
-  const handleTransaction = async (amount: number) => {
-    props.makeTransaction(props.user.username, amount);
-  };
+type State = {
+  amount: number;
+}
 
-  return (
-    <Card title="Saldo">
-      <div className="center-align">
-        <h2>{props.user.saldo}</h2>
-      </div>
-      <button className="btn" onClick={handleTransaction.bind(this, -1)}>-1</button>
-      <button className="btn right" onClick={handleTransaction.bind(this, 1)}>+1</button>
-    </Card>
-  );
-};
+class UserTransactions extends React.Component<DefaultProps, Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      amount: 0,
+    };
+    this.handleMinusPress = this.handleMinusPress.bind(this);
+    this.handlePlusPress = this.handlePlusPress.bind(this);
+  }
+
+  handleMinusPress() {
+    this.changeAmount(-1);
+  }
+
+  handlePlusPress() {
+    this.changeAmount(1);
+  }
+
+  changeAmount(amount: number) {
+    this.setState(_.update(this.state, 'amount', x => x + amount));
+    this.makeTransaction();
+  }
+
+  makeTransaction = _.debounce(async () => {
+    await this.props.makeTransaction(this.props.user.username, this.state.amount);
+    this.setState({ amount: 0 });
+  }, 2000);
+
+  render() {
+    return (
+      <Card title="Saldo">
+        <div className="center-align">
+          <h2>{this.props.user.saldo + this.state.amount}</h2>
+        </div>
+        <button className="btn" onClick={this.handleMinusPress}>-1</button>
+        <button className="btn right" onClick={this.handlePlusPress}>+1</button>
+      </Card>
+    );
+  }
+}
 
 export default UserTransactions;
